338a8f9c2bb5f09f352c20aefb2eaa1456ea39f7
davidcoats <david@getsafetycone.com>
2024-10-21 21:26:10 -0700

    Interim commit.

--- source/R5T.F0027/Code/Values/IUrls.cs
+++ source/R5T.F0027/Code/Values/IUrls.cs
@@ -8,11 +8,17 @@ namespace R5T.F0027
 	[ValuesMarker]
 	public partial interface IUrls : IValuesMarker
 	{
+        /// <summary>
+        /// The account packages URL for Nuget.org.
+        /// <para><value>https://www.nuget.org/account/Packages</value></para>
+        /// </summary>
 		public string NuGetAccountPackages => @"https://www.nuget.org/account/Packages";
-		/// <summary>
-		/// The official NuGet server URL.
-		/// Used as the source URL in push operations.
-		/// </summary>
-		public string NuGet => @"https://api.nuget.org/v3/index.json";
+
+        /// <summary>
+        /// The official NuGet server URL.
+        /// Used as the source URL in push operations.
+        /// <para><value>https://api.nuget.org/v3/index.json</value></para>
+        /// </summary>
+        public string NuGet => @"https://api.nuget.org/v3/index.json";
 	}
 }
